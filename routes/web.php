eb9a810a3002669c395abd306bbf66dd08a90153
arifpatanduk <arifpatanduk2@gmail.com>
2021-01-28 08:59:23 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dashboard;
+use App\Http\Controllers\dashboard\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index']);
